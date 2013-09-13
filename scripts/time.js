55e73fc97c4f1b9f9ea1faa8f5834bedb6d83ee7
pavel <p.varantsou@gmail.com>
2013-09-13 14:43:19 +0300

    Now using remarkjs

--- scripts/time.js
+++ scripts/time.js
@@ -1,8 +1,8 @@
-(function ($) {
-    var time = $('#time');
-    function update_time() {
-        time.text(new Date().toTimeString().split(' ')[0]);
+(function () {
+    var container = document.getElementById('time');
+    function update() {
+        container.textContent = new Date().toTimeString().split(' ')[0];
     }
-    update_time();
-    setInterval(update_time, 1000);
-}(jQuery));
+    update();
+    setInterval(update, 1000);
+}());
